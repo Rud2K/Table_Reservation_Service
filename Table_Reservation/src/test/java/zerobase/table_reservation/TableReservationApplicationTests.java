d6412f5269159793b6013c76a2422e13d072b450
Rud2K <rudgh2647k@naver.com>
2024-10-04 01:32:01 +0900

    build : 프로젝트 초기 설정 (ver 1.0)

---
+++ Table_Reservation/src/test/java/zerobase/table_reservation/TableReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package zerobase.table_reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
